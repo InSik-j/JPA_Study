06b6dcf159b4e24fdab7b44c6731a3e1f3034c99
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-22 17:13:17 +0900

    엔티티 매핑 - 기본키 매핑

--- JPA1_start/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA1_start/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -10,29 +10,31 @@ import java.util.Date;
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
 
     public Member() {
 
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
 }
