a6d2e22ae09c3295481c60c9e67a139915f6463f
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-22 20:37:13 +0900

    실전예제 2 - 연관 매핑 시작 / 양반향 연관 관계

--- JPA1_start/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA1_start/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -21,6 +23,14 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOder(this);
+    }
 
     /** getter & setter */
     public Long getId() {
