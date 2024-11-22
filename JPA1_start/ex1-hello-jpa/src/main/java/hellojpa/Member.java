dd6078e8d0aa123c98822d77c1841a69730540ff
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-22 19:28:36 +0900

    연관관계 매핑 기초 - 객체 지향 모델링 + 단방향 연관 관계

--- JPA1_start/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA1_start/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -16,8 +16,12 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -35,12 +39,12 @@ public class Member {
         this.name = name;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
 
