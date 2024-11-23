35ccb3b5de44469ce2bae6c3661444410bbfaf52
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-23 19:02:02 +0900

    실전 예제 5 - 연관관계 관리

--- JPA1_start/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA1_start/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "ORDERS")
 public class Order extends BaseEntity{
@@ -14,7 +17,7 @@ public class Order extends BaseEntity{
     @Column(name = "ODER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
@@ -23,7 +26,7 @@ public class Order extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
@@ -32,7 +35,7 @@ public class Order extends BaseEntity{
         orderItem.setOrder(this);
     }
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
