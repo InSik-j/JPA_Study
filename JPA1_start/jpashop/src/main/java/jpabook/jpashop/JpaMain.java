02c0eefe6ed0fa7bb3f1d40b599e4f56d1fa38ba
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-23 16:52:55 +0900

    실전예제 4 - 상속관계 매핑

--- JPA1_start/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA1_start/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -17,14 +18,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            em.persist(order);
-            //order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("A");
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
+            em.persist(book);
 
             tx.commit();
         }catch (Exception e){
