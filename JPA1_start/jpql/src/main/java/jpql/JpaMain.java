a65c205b5d16d68a3fc34beb318a3639edb1d410
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-24 15:50:10 +0900

    객체지향 쿼리 언어 1 - 기본 문법 / 조인

--- JPA1_start/jpql/src/main/java/jpql/JpaMain.java
+++ JPA1_start/jpql/src/main/java/jpql/JpaMain.java
@@ -14,22 +14,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            for(int i=0; i< 100; i++){
-                Member member = new Member();
-                member.setUsername("user"+i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc ", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            String query = "select m from Member m inner join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-            
+
             System.out.println("result.size() = " + result.size());
             for (Member member1 : result) {
                 System.out.println("member1 = " + member1);
