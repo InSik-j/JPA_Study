ad587aacd1e4bac4e1726e3ae723803f5faabfa4
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-24 16:07:22 +0900

    객체지향 쿼리 언어 1 - 기본 문법 / 조건식

--- JPA1_start/jpql/src/main/java/jpql/JpaMain.java
+++ JPA1_start/jpql/src/main/java/jpql/JpaMain.java
@@ -29,8 +29,17 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m inner join m.team t";
-            List<Member> result = em.createQuery(query, Member.class)
+            // 기본 case 식
+            String query1 = "select case when m.age <= 10 then '학생요금' when m.age <= 60 then '경로요금' else '일반요금' end from Member m";
+
+            // 조건식 - case 식
+                // COALESCE: 하나씩 조회해서 null이 아니면 반환
+            String query2 = "select coalesce(m.username,'이름 없는 회원') from Member m";
+
+                // NULLIF: 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
+            String query3 = "select NULLIF(m.username, '관리자') from Member m";
+
+            List<Member> result = em.createQuery(query1, Member.class)
                     .getResultList();
 
             System.out.println("result.size() = " + result.size());
