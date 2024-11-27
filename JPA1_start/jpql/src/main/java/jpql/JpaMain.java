c7011731b8d53134a3f650f586545aeb1c3aff8d
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-11-27 13:11:38 +0900

    객체지향 쿼리 언어 2 - 중급 문법 /  벌크 연산

--- JPA1_start/jpql/src/main/java/jpql/JpaMain.java
+++ JPA1_start/jpql/src/main/java/jpql/JpaMain.java
@@ -37,16 +37,16 @@ public class JpaMain {
             member3.setTeam(team2);
             em.persist(member3);
 
-            em.flush();
+            // flush() 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+            System.out.println("resultCount = " + resultCount);
+
+            System.out.println("member1.getAge() = " + member1.getAge());
             em.clear();
-
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "user1")
-                    .getResultList();
-
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             tx.commit();
         }catch (Exception e){
