374c8f8dc3f73e31faa4ef2af71e551a4898d03c
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-12-11 15:53:21 +0900

    API 개발 기본 / 회원 수정 API

--- Spring_JPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ Spring_JPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,18 +4,17 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
-    
+
     /** 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다. */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -35,6 +34,28 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /** 수정 API */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberResponseV2(@PathVariable("id") Long id,
+                                                       @RequestBody @Valid UpdateMemberRequset request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findMember(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequset {
+        public String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        public Long id;
+        public String name;
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
@@ -49,4 +70,6 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
+
 }
