583dfc1e0080fab8ba4f468fd2a99f81a8d3c149
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-12-19 15:26:47 +0900

    예제 도메인 모델 / 예제 도메인 모델 및 동작 확인

--- Spring_datajpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Spring_datajpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,21 +1,45 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
-@Getter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
 
-    protected Member() {
-    }
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
